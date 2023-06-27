802a9d1dab64e550120919a4b06c5c76d02be22a
Jonry Simbolon <jonrysimbolon19@gmail.com>
2023-06-27 22:43:30 +0700

    Make change from model to entity

--- app/src/main/java/com/jonrysimbolon/testskillmovie/data/local/entity/RkurEntity.kt
+++ app/src/main/java/com/jonrysimbolon/testskillmovie/data/local/entity/RkurEntity.kt
@@ -1,10 +1,12 @@
-package com.jonrysimbolon.testskillmovie.repository.local.model
+package com.jonrysimbolon.testskillmovie.data.local.entity
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.jonrysimbolon.testskillmovie.utils.rkur_tbl
 
 @Entity(tableName = rkur_tbl)
-data class RkurModel(
+data class RkurEntity(
     @PrimaryKey val id: String,
+    val prevKey: Int?,
+    val nextKey: Int?
 )
