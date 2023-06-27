325bfa47a9854dccc06e3865c0a9fb913848dbcb
Jonry Simbolon <jonrysimbolon19@gmail.com>
2023-06-27 12:24:59 +0700

    Initial optimize to SplashFragment.kt

--- app/src/main/java/com/jonrysimbolon/testskillmovie/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/jonrysimbolon/testskillmovie/ui/splash/SplashFragment.kt
@@ -5,16 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.jonrysimbolon.testskillmovie.R
 
 class SplashFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = SplashFragment()
-    }
-
-    private lateinit var viewModel: SplashViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,10 +16,5 @@ class SplashFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_splash, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(SplashViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
 
 }
