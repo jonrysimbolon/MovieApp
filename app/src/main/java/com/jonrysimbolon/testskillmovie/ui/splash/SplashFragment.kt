cc28b0227a8aa5007ab3bb00fe15188d26a88b80
Jonry Simbolon <jonrysimbolon19@gmail.com>
2023-06-27 22:49:04 +0700

    Optimize some variable and i want to make if all categories completed to get and save, it will navigate to HomeFragment

--- app/src/main/java/com/jonrysimbolon/testskillmovie/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/jonrysimbolon/testskillmovie/ui/splash/SplashFragment.kt
@@ -7,16 +7,17 @@ import android.view.ViewGroup
 import android.view.animation.AnimationUtils
 import androidx.annotation.AnimRes
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.findNavController
 import com.jonrysimbolon.testskillmovie.R
 import com.jonrysimbolon.testskillmovie.databinding.FragmentSplashBinding
+import kotlinx.coroutines.launch
+import org.koin.androidx.viewmodel.ext.android.activityViewModel
 
 class SplashFragment : Fragment() {
 
     private val binding by lazy { FragmentSplashBinding.inflate(layoutInflater) }
-    private val delayMillis: Long = 2000
-    @AnimRes
-    private val animateSplashImage = R.anim.side_slide
+    private val viewModel: SplashViewModel by activityViewModel()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,10 +28,20 @@ class SplashFragment : Fragment() {
         binding.splashImage.startAnimation(
             AnimationUtils.loadAnimation(requireContext(), animateSplashImage)
         )
-        view.let {
-            it.postDelayed({
-                it.findNavController().navigate(R.id.action_splashFragment_to_homeFragment)
-            }, delayMillis)
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            val isCategoriesLoaded = viewModel.getAllCategories()
+            if (isCategoriesLoaded) {
+                view.findNavController().navigate(
+                    toHomeFragment
+                )
+            }
         }
+    }
+
+    companion object {
+        @AnimRes
+        private val animateSplashImage = R.anim.side_slide
+        private val toHomeFragment = R.id.action_splashFragment_to_homeFragment
     }
 }
