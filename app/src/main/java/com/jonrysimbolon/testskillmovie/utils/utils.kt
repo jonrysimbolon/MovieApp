8f1f0569678199bd246c50c9f621287789cc0152
Jonry Simbolon <jonrysimbolon19@gmail.com>
2023-06-27 18:41:28 +0700

    Adding name of entity

--- app/src/main/java/com/jonrysimbolon/testskillmovie/utils/utils.kt
+++ app/src/main/java/com/jonrysimbolon/testskillmovie/utils/utils.kt
@@ -1,1 +1,7 @@
 package com.jonrysimbolon.testskillmovie.utils
+
+const val category_tbl = "category_tbl"
+const val movie_tbl = "movie_tbl"
+const val user_review_tbl = "user_review_tbl"
+const val rkur_tbl = "remote_keys_user_review_tbl"
+const val rkm_tbl = "remote_keys_movie_tbl"
