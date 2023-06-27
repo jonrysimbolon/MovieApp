07d13efc53b99caaa44124aa588350a8a482b3a9
Jonry Simbolon <jonrysimbolon19@gmail.com>
2023-06-27 11:59:03 +0700

    Initial splash

---
+++ app/src/main/java/com/jonrysimbolon/testskillmovie/ui/splash/SplashFragment.kt
@@ -0,0 +1,32 @@
+package com.jonrysimbolon.testskillmovie.ui.splash
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.jonrysimbolon.testskillmovie.R
+
+class SplashFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = SplashFragment()
+    }
+
+    private lateinit var viewModel: SplashViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(SplashViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/jonrysimbolon/testskillmovie/ui/splash/SplashViewModel.kt
@@ -0,0 +1,7 @@
+package com.jonrysimbolon.testskillmovie.ui.splash
+
+import androidx.lifecycle.ViewModel
+
+class SplashViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
