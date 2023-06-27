da6442360907555716801058fc03ad55cb04198c
Jonry Simbolon <jonrysimbolon19@gmail.com>
2023-06-27 22:44:09 +0700

    add movie_database and videos to utils.kt

--- app/src/main/java/com/jonrysimbolon/testskillmovie/utils/utils.kt
+++ app/src/main/java/com/jonrysimbolon/testskillmovie/utils/utils.kt
@@ -1,7 +1,9 @@
 package com.jonrysimbolon.testskillmovie.utils
 
+const val movie_database = "movie_database"
 const val category_tbl = "category_tbl"
 const val movie_tbl = "movie_tbl"
 const val user_review_tbl = "user_review_tbl"
 const val rkur_tbl = "remote_keys_user_review_tbl"
 const val rkm_tbl = "remote_keys_movie_tbl"
+const val videos = "videos"
