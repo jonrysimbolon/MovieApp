321a26290db4976af36d1b1034bb62a305b8797b
Jonry Simbolon <jonrysimbolon19@gmail.com>
2023-06-27 18:44:23 +0700

    Test spinner with custom array

--- app/src/main/java/com/jonrysimbolon/testskillmovie/ui/home/HomeFragment.kt
+++ app/src/main/java/com/jonrysimbolon/testskillmovie/ui/home/HomeFragment.kt
@@ -5,15 +5,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.jonrysimbolon.testskillmovie.R
+import com.jonrysimbolon.testskillmovie.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+
+    private val binding by lazy { FragmentHomeBinding.inflate(layoutInflater) }
+    private val list = listOf("Mangga", "Jambu", "gula")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View = binding.root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.apply {
+            categorySV.setItems(list)
+        }
     }
 
 }
