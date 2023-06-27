0e24584f7f3b1d859e30706a6eba9c75d0c57a50
Jonry Simbolon <jonrysimbolon19@gmail.com>
2023-06-27 22:46:28 +0700

    add suspend method so i can add category process to SplashFragment.kt from SplashViewModel.kt

--- app/src/main/java/com/jonrysimbolon/testskillmovie/ui/splash/SplashViewModel.kt
+++ app/src/main/java/com/jonrysimbolon/testskillmovie/ui/splash/SplashViewModel.kt
@@ -1,7 +1,15 @@
 package com.jonrysimbolon.testskillmovie.ui.splash
 
 import androidx.lifecycle.ViewModel
+import com.jonrysimbolon.testskillmovie.repository.category.CategoryRepository
 
-class SplashViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class SplashViewModel constructor(
+    private val categoryRepository: CategoryRepository
+) : ViewModel() {
+    suspend fun getAllCategories(): Boolean = try {
+        categoryRepository.getAllCategories()
+        true
+    } catch (e: Exception) {
+        false
+    }
 }
