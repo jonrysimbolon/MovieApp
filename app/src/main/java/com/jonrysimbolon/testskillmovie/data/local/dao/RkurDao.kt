c12c6bb89ca13928ebde9b84b93d96f2c7660b41
Jonry Simbolon <jonrysimbolon19@gmail.com>
2023-06-27 22:43:25 +0700

    Make change from model to entity

--- app/src/main/java/com/jonrysimbolon/testskillmovie/data/local/dao/RkurDao.kt
+++ app/src/main/java/com/jonrysimbolon/testskillmovie/data/local/dao/RkurDao.kt
@@ -1,16 +1,18 @@
-package com.jonrysimbolon.testskillmovie.repository.local.dao
+package com.jonrysimbolon.testskillmovie.data.local.dao
 
+import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import com.jonrysimbolon.testskillmovie.repository.local.model.RkurModel
+import com.jonrysimbolon.testskillmovie.data.local.entity.RkurEntity
 
+@Dao
 interface RkurDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertAll(remoteKey: List<RkurModel>)
+    suspend fun insertAll(remoteKey: List<RkurEntity>)
 
     @Query("SELECT * FROM remote_keys_user_review_tbl WHERE id = :id")
-    suspend fun getRemoteKeysId(id: String): RkurModel?
+    suspend fun getRemoteKeysId(id: String): RkurEntity?
 
     @Query("DELETE FROM remote_keys_user_review_tbl")
     suspend fun deleteRemoteKeys()
