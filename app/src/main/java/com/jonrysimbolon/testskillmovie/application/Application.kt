83e3e4e35d7b44049b5fe5ca1ef70642738cfdd5
Jonry Simbolon <jonrysimbolon19@gmail.com>
2023-06-27 22:13:31 +0700

    Adding Application.kt cause used koin as dependency injection

---
+++ app/src/main/java/com/jonrysimbolon/testskillmovie/application/Application.kt
@@ -0,0 +1,27 @@
+package com.jonrysimbolon.testskillmovie.application
+
+import android.app.Application
+import com.jonrysimbolon.testskillmovie.BuildConfig
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.androidx.fragment.koin.fragmentFactory
+import org.koin.core.context.startKoin
+
+class Application : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidLogger()
+            androidContext(this@Application)
+            fragmentFactory()
+            modules(
+                gsonModule,
+                remoteModule(BuildConfig.Base_Url),
+                localModule,
+                glideModule,
+                repositoryModule,
+                viewModelModule,
+            )
+        }
+    }
+}
